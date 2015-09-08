8e8f58a8924b5baf0495bf71cec3ca18b06c0a23
Jacob Lin <jacoblin@berkeley.edu>
2015-09-08 14:06:57 -0700

    Added all ITIS files.

--- ITIS/ViewController.swift
+++ ITIS/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func goToLibrary(sender: UIButton) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = .PhotoLibrary
+        presentViewController(image, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func goToCamera(sender: UIButton) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.Camera
+        presentViewController(image, animated: true, completion: nil)
+    }
+    
 }
 
